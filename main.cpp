6cb60df071a075285c4319250e83d2d3e5ccdad5
Dreamerche <martitopp14@gmail.com>
2024-11-24 15:10:04 +0200

    first change

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+class Simulation{
+    private:
+        Room room;
+        std::queue<Student> waiting;
+        int currentTime=8;
+        Seat* seats;
+        Seat* usableSeats;//
+
+
+
+
+};
+
+void startSimulation(){
+    Simulation sim;
+}
+
+int main(){
+    startSimulation();
+    return 0;
+}
